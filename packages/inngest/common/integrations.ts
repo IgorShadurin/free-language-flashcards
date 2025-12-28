44836a3d6e3de4723535859273b65d5040757bc9
Igor Shadurin <igor.shadurin@gmail.com>
2025-12-29 01:26:31 +0300

    feat: fix functions for vercel

--- packages/inngest/common/integrations.ts
+++ packages/inngest/common/integrations.ts
@@ -1,8 +1,16 @@
-export const importIntegration = async (path: string) => {
+export async function importIntegration(
+  path: "quizlet",
+): Promise<typeof import("../../integrations/quizlet")>;
+export async function importIntegration(
+  path: "quizlet/inngest",
+): Promise<typeof import("../../integrations/quizlet/inngest")>;
+export async function importIntegration(path: string) {
   switch (path) {
+    case "quizlet":
+      return await import("../../integrations/quizlet");
     case "quizlet/inngest":
       return await import("../../integrations/quizlet/inngest");
     default:
       throw new Error(`Unknown integration: ${path}`);
   }
-};
+}
--- packages/trpc/integrations.ts
+++ packages/trpc/integrations.ts
@@ -1,8 +1,11 @@
-export const importIntegration = async (path: string) => {
+export async function importIntegration(
+  path: "quizlet",
+): Promise<typeof import("../integrations/quizlet")>;
+export async function importIntegration(path: string) {
   switch (path) {
     case "quizlet":
       return await import("../integrations/quizlet");
     default:
       throw new Error(`Unknown integration: ${path}`);
   }
-};
+}
