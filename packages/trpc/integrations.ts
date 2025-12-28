d9b0f4fe685bb33b26b77c0f995fed15dd6195ad
Igor Shadurin <igor.shadurin@gmail.com>
2025-12-29 01:23:58 +0300

    feat: fix functions for vercel

--- packages/trpc/integrations.ts
+++ packages/trpc/integrations.ts
@@ -1,4 +1,8 @@
 export const importIntegration = async (path: string) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return await import(`../integrations/${path}`);
+  switch (path) {
+    case "quizlet":
+      return await import("../integrations/quizlet");
+    default:
+      throw new Error(`Unknown integration: ${path}`);
+  }
 };
